d9110b5602171f1b51349199b1863a6c6905b251
Jack Hurt <jackwhurt@gmail.com>
2023-02-14 21:46:06 +0000

    Removed rest of unnecessary code in cal event

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,53 +1,48 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  ViewChild,
-  TemplateRef,
-  OnInit,
-} from '@angular/core';
-import {
-  startOfDay,
-  endOfDay,
-  subDays,
-  addDays,
-  endOfMonth,
-  isSameDay,
-  isSameMonth,
-  addHours,
-} from 'date-fns';
+import { Component, ChangeDetectionStrategy, ViewChild, TemplateRef, OnInit } from '@angular/core';
+import { isSameDay, isSameMonth, } from 'date-fns';
 import { Subject } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import {
-  CalendarEvent,
-  CalendarEventAction,
-  CalendarEventTimesChangedEvent,
-  CalendarView,
-} from 'angular-calendar';
-import { EventColor } from 'calendar-utils';
+import { CalendarEvent, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
 import { CalendarEventService } from 'src/app/services/calendar-event.service';
 
-const colors: Record<string, EventColor> = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3',
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF',
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA',
-  },
-  purple: {
-    primary: '#9114b8',
-    secondary: '#9114b8',
-  },
-  green: {
-    primary: '#17b814',
-    secondary: '#17b814',
-  },
-};
+// TODO: Delete
+// const colors: Record<string, EventColor> = {
+//   red: {
+//     primary: '#ad2121',
+//     secondary: '#FAE3E3',
+//   },
+//   blue: {
+//     primary: '#1e90ff',
+//     secondary: '#D1E8FF',
+//   },
+//   yellow: {
+//     primary: '#e3bc08',
+//     secondary: '#FDF1BA',
+//   },
+//   purple: {
+//     primary: '#9114b8',
+//     secondary: '#9114b8',
+//   },
+//   green: {
+//     primary: '#17b814',
+//     secondary: '#17b814',
+//   },
+// };
+// events: CalendarEvent[] = [
+//   {
+//     start: subDays(startOfDay(new Date()), 1),
+//     end: addDays(new Date(), 1),
+//     title: 'A 3 day event',
+//     color: { ...colors['red'] },
+//     actions: this.actions,
+//     allDay: true,
+//     resizable: {
+//       beforeStart: true,
+//       afterEnd: true,
+//     },
+//     draggable: true,
+//   }
+// ];
 
 @Component({
   selector: 'app-calendar',
@@ -59,58 +54,19 @@ export class CalendarComponent implements OnInit {
   @ViewChild('modalContent', { static: true }) modalContent: TemplateRef<any> | undefined;
 
   view: CalendarView = CalendarView.Month;
-
   CalendarView = CalendarView;
-
   viewDate: Date = new Date();
-
   events: CalendarEvent[] = [];
-  eventsObj: any[] = [];
+  refresh = new Subject<void>();
 
   modalData: {
     action: string;
     event: CalendarEvent;
   } | undefined;
 
-  actions: CalendarEventAction[] = [
-    {
-      label: '<i class="fas fa-fw fa-pencil-alt"></i>',
-      a11yLabel: 'Edit',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.handleEvent('Edited', event);
-      },
-    },
-    {
-      label: '<i class="fas fa-fw fa-trash-alt"></i>',
-      a11yLabel: 'Delete',
-      onClick: ({ event }: { event: CalendarEvent }): void => {
-        this.events = this.events.filter((iEvent) => iEvent !== event);
-        this.handleEvent('Deleted', event);
-      },
-    },
-  ];
-
-  refresh = new Subject<void>();
-
-  // events: CalendarEvent[] = [
-  //   {
-  //     start: subDays(startOfDay(new Date()), 1),
-  //     end: addDays(new Date(), 1),
-  //     title: 'A 3 day event',
-  //     color: { ...colors['red'] },
-  //     actions: this.actions,
-  //     allDay: true,
-  //     resizable: {
-  //       beforeStart: true,
-  //       afterEnd: true,
-  //     },
-  //     draggable: true,
-  //   }
-  // ];
-
   activeDayIsOpen: boolean = true;
 
-  constructor(private modal: NgbModal, private calendarEventService: CalendarEventService) {}
+  constructor(private modal: NgbModal, private calendarEventService: CalendarEventService) { }
 
   ngOnInit() {
     const date: Date = new Date();
@@ -118,7 +74,7 @@ export class CalendarComponent implements OnInit {
     const month: string = String(date.getMonth() + 1).padStart(2, '0');
 
     this.calendarEventService.getCalendarEventsByMonth(year + '-' + month).subscribe((eventsToSet) => {
-      if(eventsToSet){
+      if (eventsToSet) {
         eventsToSet.map((entry) => entry.start = new Date(entry.start));
         this.events = this.events.concat(eventsToSet);
         this.refresh.next();
@@ -155,20 +111,6 @@ export class CalendarComponent implements OnInit {
       }
       return iEvent;
     });
-    this.handleEvent('Dropped or resized', event);
-  }
-
-  handleEvent(action: string, event: CalendarEvent): void {
-    this.modalData = { event, action };
-    this.modal.open(this.modalContent, { size: 'lg' });
-  }
-
-  deleteEvent(eventToDelete: CalendarEvent) {
-    this.events = this.events.filter((event) => event !== eventToDelete);
-  }
-
-  setView(view: CalendarView) {
-    this.view = view;
   }
 
   closeOpenMonthViewDay() {
