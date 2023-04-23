0815f50d8cb325708b8676086912069898988f13
Jack Hurt <jackwhurt@gmail.com>
2023-04-23 19:26:36 +0100

    Added fill date

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -9,22 +9,86 @@ import { CvSection } from 'src/app/interfaces/cv';
 export class CvComponent {
   cvSections: CvSection[] = [
     {
-      title: 'SectionTitle',
+      title: 'Skills',
       cvItems: [{
-        title: 'CvTitle',
-        description: 'Description',
-        location: 'Location',
-        duration: 'Duration',
         bulletPoints: ['point1', 'point2']
       }]
     },
     {
-      title: 'SectionTitle',
+      title: 'Experience',
+      cvItems: [
+        {
+          title: 'Trainee Software Engineer',
+          location: 'Kainos',
+          duration: 'Aug 2022 - Aug 2023',
+          bulletPoints: [
+            'Currently developing a web-app using Angular to digitalise a multitude of workflows within hospitals',
+            'Led original initiative to raise unit test coverage, resulting in an increase of 100%',
+            'Mentoring the apprentice with efforts such as pair programming sessions and weekly catch-up calls',
+            'Part of a 7 week training academy which covered topics such as: full-stack development, REST APIs, databases, functional & non-functional testing, quality code and agile methodologies',
+          ]
+        },
+        {
+          title: 'CvTitle',
+          description: 'Description',
+          location: 'Location',
+          duration: 'Duration',
+          bulletPoints: ['point1', 'point2']
+        }
+      ]
+    },
+    {
+      title: 'Education',
+      cvItems: [
+        {
+          title: 'CvTitle',
+          description: 'Description',
+          location: 'Location',
+          duration: 'Duration',
+          bulletPoints: ['point1', 'point2']
+        },
+        {
+          title: 'CvTitle',
+          description: 'Description',
+          location: 'Location',
+          duration: 'Duration',
+          bulletPoints: ['point1', 'point2']
+        }
+      ]
+    },
+    {
+      title: 'Projects',
+      cvItems: [
+        {
+          title: 'CvTitle',
+          description: 'Description',
+        },
+        {
+          title: 'CvTitle',
+          description: 'Description',
+        },
+      ]
+    },
+    {
+      title: 'Leadership Experience',
+      cvItems: [
+        {
+          title: 'CvTitle',
+          location: 'Location',
+          duration: 'Duration',
+          bulletPoints: ['point1', 'point2']
+        },
+        {
+          title: 'CvTitle',
+          location: 'Location',
+          duration: 'Duration',
+          bulletPoints: ['point1', 'point2']
+        },
+      ]
+    },
+    {
+      title: 'Achievements',
       cvItems: [{
-        title: 'CvTitle',
-        description: 'Description',
-        location: 'Location',
-        duration: 'Duration',
         bulletPoints: ['point1', 'point2']
       }]
     }
