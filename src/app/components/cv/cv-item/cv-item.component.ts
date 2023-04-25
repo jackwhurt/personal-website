1c575bc51310d33138e3cc35304af67cf13f40db
Jack Hurt <jackwhurt@gmail.com>
2023-04-25 18:47:33 +0100

    Unique identifiers for modals

--- src/app/components/cv/cv-item/cv-item.component.ts
+++ src/app/components/cv/cv-item/cv-item.component.ts
@@ -15,4 +15,14 @@ export class CvItemComponent {
   @Input() bulletPoints?: Point[];
   @Input() list?: string[];
   @ViewChild('cvModal') cvModalComponent!: CvModalComponent;
+
+  hash(str: string): number {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i);
+      hash = (hash << 5) - hash + char;
+      hash |= 0;
+    }
+    return hash;
+  }
 }
