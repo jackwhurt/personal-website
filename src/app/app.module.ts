4c7ec2bd9a4b170a06973368a10f90321681e7d6
Jack Hurt <jackwhurt@gmail.com>
2023-04-30 18:25:36 +0100

    Reorganised cv & gym-tracker components and used modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { FlatpickrModule } from 'angularx-flatpickr';
-
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { GymTrackerComponent } from './components/gym-tracker/gym-tracker.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { PodiumComponent } from './components/podium/podium.component';
-import { CvComponent } from './components/cv/cv.component';
-import { CvItemComponent } from './components/cv/cv-item/cv-item.component';
-import { CvSectionComponent } from './components/cv/cv-section/cv-section.component';
-import { CvModalComponent } from './components/cv/cv-modal/cv-modal.component';
+import { GymTrackerModule } from './components/gym-tracker/gym-tracker.module';
+import { CvModule } from './components/cv/cv.module';
 
 @NgModule({
   declarations: [
@@ -29,28 +16,14 @@ import { CvModalComponent } from './components/cv/cv-modal/cv-modal.component';
     NavbarComponent,
     HomeComponent,
     FooterComponent,
-    GymTrackerComponent,
-    CalendarComponent,
     PageNotFoundComponent,
-    PodiumComponent,
-    CvComponent,
-    CvItemComponent,
-    CvSectionComponent,
-    CvModalComponent,
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    NgbModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
+    BrowserModule,
+    CvModule,
+    GymTrackerModule,
     FlatpickrModule.forRoot(),
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory,
-    })
   ],
   providers: [],
   bootstrap: [AppComponent]
